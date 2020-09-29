6e99a06c5a3494ca89b3debf9cc648aab2b3550f
AilenGomez <32937466+AilenGomez@users.noreply.github.com>
2020-09-28 22:00:18 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Curriculum.Services;
+using System.Web.Mvc;
+
+namespace cv.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Action()
+        {
+            var domain = new Curriculum.Services.DataBaseConnection();
+            //domain.CrearConexion();
+           var conexion = domain.CrearConexion();
+            var metodo = new Curriculum.Services.HomeServices();
+            metodo.ExecuteSP(conexion);
+            //domain.ExecuteCommand();
+
+            return View();
+        }
+    }
+}
